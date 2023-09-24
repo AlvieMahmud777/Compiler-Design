efa387e14da0542357858a7563757a72c73cd6d7
Alvie Mahmud Shoumin <145887149+AlvieMahmud777@users.noreply.github.com>
2023-09-24 23:47:07 +0600

    Update task2 (2).cpp

--- task2 (2).cpp
+++ task2 (2).cpp
@@ -1,31 +1,22 @@
 //token categorization
 #include<iostream>
 #include<string>
-
 using namespace std;
-
 int main() {
-
 string expression;
 string operators;
-
-     cout << "Enter an expression: ";
-    cin >> expression;
-
+cout << "Enter an expression: ";
+     cin >> expression;
 for (char ch : expression) {
 if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
-    {
-            operators += ch;
+    { operators += ch;
         }
     }
 
-    if (!operators.empty()) {
-
-        cout << "Operators are: " << operators << endl;
-    } else {
-        cout << "No operators found" << endl;
+ if (!operators.empty()) {
+cout << "Operators are: " << operators << endl;
+ } else {
+cout << "No operators found" << endl;
 }
-
-
-    return 0;
+ return 0;
 }
